2fe49a1858bdf2e14e966b6c26c4589158765b2f
Joshua <psyjr4@nottingham.ac.uk>
2020-01-28 23:21:21 +0000

    Added the post method on right page

--- src/public/js/image.js
+++ src/public/js/image.js
@@ -19,6 +19,31 @@ window.addEventListener('load', function() {
         }
     })
 
+
+    function postRecipe(name, description, imageUrl) {
+        console.log("Hi");
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "http://localhost:5000/api/addrecipe");
+        var data = {};
+        data.name = name;
+        data.description = description;
+        data.imageUrl = imageUrl;
+
+    
+        var json = JSON.stringify(data);
+    
+        console.log(json);
+    
+        xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8')
+    
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.responseText);
+            }
+        }
+        xhttp.send(json);
+    }
+
     function validType() {
         console.log("Change");
         return true;
