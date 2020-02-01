640385cafac895153e561f60d1cc9e5b3610f926
Joshua <psyjr4@nottingham.ac.uk>
2020-02-01 22:46:16 +0000

    Tidied up and improved styling

--- src/public/js/drag-and-drop.js
+++ src/public/js/drag-and-drop.js
@@ -1,18 +1,15 @@
 var data = [];
-var dragFrom = "drag3";
-var dragTo = "drag3";
+var dragFrom = "drag0";
+var dragTo = "drag0";
 
 var originalPosition;
 
 window.addEventListener('load', function(event) {
     var addButton = document.getElementById('add-button');
     addButton.addEventListener('click', function() {
-        console.log("Method addition");
-
         data.push("");
-        var thisId = data.length - 1
-
         var methodStep = document.createElement('div');
+        var thisId = data.length - 1
         methodStep.id = `drag${thisId}`;
         methodStep.setAttribute('draggable', true);
         methodStep.setAttribute('ondragstart', 'drag(event)');
@@ -21,24 +18,16 @@ window.addEventListener('load', function(event) {
         methodStep.setAttribute('ondragleave', 'noLine(event)');
         methodStep.classList.add("ingredient-item");
         methodStep.classList.add("create-step-row");
-
         methodStep.innerHTML = `<input id="input${thisId}" class="recipe-step input-detail">`
-
         document.getElementById('drag-and-drop-container').appendChild(methodStep);
-
         document.getElementById(`input${thisId}`).addEventListener('input', function(event) {
-            console.log("Change")
             id = event.currentTarget.id.replace('input', '');
             data[id] = document.getElementById(`input${thisId}`).value;
         });
     });
-
 });
 
 function reorderData() {
-
-    console.log(data);
-
     dragFromInt = dragFrom.replace('drag', '');
     dragToInt = dragTo.replace('drag', '');
 
@@ -54,7 +43,6 @@ function reorderData() {
 function allowDrop(e) {
     e.preventDefault();
     var divId = e.currentTarget.id;
-    
     console.log(originalPosition);
     if (e.clientX > originalPosition) {
         document.getElementById(divId).classList.add('below');
@@ -63,10 +51,6 @@ function allowDrop(e) {
         document.getElementById(divId).classList.add('above');
         document.getElementById(divId).classList.remove('below');
     }
-}
-
-function underline(e) {
-    console.log(divId);
 }
 
 function noLine(e) {
@@ -78,24 +62,15 @@ function noLine(e) {
 function drag(e) {
     originalPosition = e.clientX;
     e.dataTransfer.setData("text", e.currentTarget.id);
-    console.log(e.currentTarget.id);
     dragFrom = e.currentTarget.id;
-    console.log(e.clientX);
     document.getElementById(dragFrom).classList.add('on-drag');
-
 }
 
 function drop(e) {
     e.preventDefault();
-    var data = e.preventDefault();
-    var data = e.dataTransfer.getData("text");
     var divId = e.currentTarget.id;
-    console.log(e.currentTarget.id);
     dragTo = e.currentTarget.id;
-
     document.getElementById(dragFrom).classList.remove('on-drag');
-
-    
     document.getElementById(divId).classList.remove('below');
     document.getElementById(divId).classList.remove('above');
     reorderData();
